221fef70ecd5910705820aad155aa7637096e5b5
Mightnire <72493627+Mightnire@users.noreply.github.com>
2021-12-11 15:30:19 +0800

    11

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ For jsjzyk.36ve.com
 ### 安装依赖包
 `pip install -r requirements.txt`
 ### 输入课程ID
-`https://jsjzyk.36ve.com/?q=items/student/study/76723
+`https://jsjzyk.36ve.com/?q=items/student/study/76723`
+
 末尾的76723就是课程ID
-`
+
+![img.png](others/img.png)
 ### Enjoy it！
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 import time, csv
 
 request = requests.Session()
+def logging(typ='-', text='-'):
+    print(f'[{typ}] {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())} {text}')
 
 def replace_sym(data):
     data = str(data)
@@ -15,13 +17,10 @@ def replace_sym(data):
 
 def login(username, password, ):
     try:
-        print("打开登录页面")
-        login_part = BeautifulSoup(request.get(wzk_url, headers=header).text, features="html.parser").find(name='a',
-                                                                                                           attrs={
-                                                                                                               'class': 'btn btn-info btn-small'}).get(
-            'href')
+        logging("i","正在打开登录页面...")
+        login_part = BeautifulSoup(request.get(wzk_url, headers=header).text, features="html.parser").find(name='a',attrs={'class': 'btn btn-info btn-small'}).get('href')
         login_url = wzk_url + login_part
-        print("构造登录请求")
+        logging("i","构造登录请求...")
         login_csrf_token = BeautifulSoup(request.get(login_url, headers=header).text, features="html.parser").find(
             name='input', attrs={'name': 'csrf_token', 'id': 'csrf_token'}).get('value')
         pwd = hashlib.md5(password.encode(encoding='UTF-8')).hexdigest()
@@ -34,18 +33,18 @@ def login(username, password, ):
         data = {"datainfo": base64.b64encode(str(data).encode(
             encoding='UTF-8')).decode(encoding='UTF-8')}
         login_post_url = wzk_url + '/?q=user_login_submit'
-        print("提交登录请求")
+        logging("i","提交登录请求")
         login = request.post(login_post_url, data=data, headers=header).text
         login = login.encode('utf8')[3:].decode('utf8')
-        print(login)
+        logging("!",f'登录状态：{login}')
         if r'\u7528\u6237\u540d\u6216\u5bc6\u7801\u9519\u8bef' in login:
-            print("用户名或密码错误，请重新输入！")
+            logging("!","用户名或密码错误，请重新输入！")
         elif r'\u7531\u4e8e\u60a8\u8fde\u7eed\u8f93\u5165\u5bc6' in login:
-            print("由于您连续输入密码错误，账号将会锁定5分钟，请5分钟后再登录！")
+            logging("!","由于您连续输入密码错误，账号将会锁定5分钟，请5分钟后再登录！")
         elif r"\u9a8c\u8bc1\u672a\u901a\u8fc7\uff0c\u8bf7\u91cd" in login:
-            print("csrf验证失败！")
+            logging("!","csrf验证失败！")
         else:
-            print(f'用户 {json.loads(login)["userid"]} 登录成功')
+            logging("*",f'用户 {json.loads(login)["userid"]} 登录成功')
             return json.loads(login)["userid"]
     except:
         return False
@@ -53,7 +52,7 @@ def login(username, password, ):
 
 def get_video_link(courseid):
     for i in range(3):
-        print(f'第{i + 1}次尝试，课程ID = {courseid}')
+        logging("i",f'第 {i + 1}/3 次尝试，课程ID为 {courseid}')
         url = wzk_url + '/?q=items/student/study/' + courseid
         header = {"Host": "jsjzyk.36ve.com",
                   "Connection": "keep-alive",
@@ -73,10 +72,10 @@ def get_video_link(courseid):
         all_link = [wzk_url + link_element.find('a').get('href') for link_element in all_content_element if
                     link_element.find(
                         'radius bg-img-half') != '' or link_element.find('class="radius "') != '']
-        print(f'获取了{len(all_link)}条链接。')
+        logging("*",f'获取了 {len(all_link)} 条链接。')
         if len(all_link) > 0:
             return all_link
-    print("未能获取学习条目链接 ：(")
+    logging("!","没有要学习的项目！")
     return []
 
 
@@ -105,42 +104,46 @@ def get_course_link(userid):
     course_id = []
     if userid:
         while 1:
-            i = input("输入课程ID（留空按回车结束）：")
+            logging("!", "输入课程ID（留空按回车结束）")
+            i = input(">")
             if i.isnumeric():
                 course_id.append(str(int(i)))
             else:
-                print(course_id)
+                logging("*",f'共 {len(course_id)} 条课程。')
                 return course_id
 
 
 if __name__ == '__main__':
     wzk_url = 'http://jsjzyk.36ve.com'
-    username = str(input('用户名：'))
-    password = str(input('密码:'))
+    logging("!", "输入用户名")
+    username = str(input('>'))
+    logging("!", "输入密码")
+    password = str(input('>'))
     header = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"}
     try:
         userid = login(username, password)
         if userid == 0:
-            raise "登录失败！"
+            logging("!", "登录失败！")
+            raise "loginException"
     except:
         sys.exit()
     conf = 0
     while conf != 9:
-        conf = int(input('接下来做什么呢？\n\t1.刷课\n\t2.生成待刷项链接\n\t'))
+        logging("!","等待用户选择！")
+        conf = int(input('接下来做什么呢？\n\t1.刷课\n\t2.生成待刷项链接\n\t>'))
         if conf == 1:
-            print("DO!")
+            logging("-","开始运行！")
             with open("test.txt", 'r') as file:
                 f = csv.reader(file)
                 for _ in f:
-                    print("-")
+                    logging("!","切换课程...")
                     for i in range(1, len(_)):
-                        while i % 50 == 0:
-                            print("为了您的安全。正在重新登录。")
+                        while i % 66 == 0:
+                            logging("*","重新登录中")
                             login(username, password)
                             break
-                        print(f'[{i}/{len(_) - 1}] {_[i]}')
-                        time.sleep(0.2)
+                        logging("~",f'[{i}/{len(_) - 1}] {_[i]}')
                         do_auto_new(link=_[i], userid=userid, courseid=_[0])
         elif conf == 2:
             course_id = get_course_link(userid)
